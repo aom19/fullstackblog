941ef9ca1b7d33ea362b216765865aae7dbec520
sub_zero <ostapovici.anton@gmail.com>
2020-11-25 11:48:45 +0200

    Set home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import trending from "../assets/mocks/trending";
-import { PostMasonry } from "../components/common/index";
+import { PostMasonry, MasonryPost } from "../components/common/index";
 import featured from "../assets/mocks/featured";
 
 const trendingConfig = {
@@ -8,7 +8,21 @@ const trendingConfig = {
     gridArea: "1 / 2 / 3 / 3",
   },
 };
-const featuredConfig = {};
+const featuredConfig = {
+  0: {
+    gridArea: "1 / 1 / 2 / 3",
+    height: "300px",
+    width: "100%",
+  },
+  1: {
+    height: "300px",
+  },
+  3: {
+    height: "630px",
+    marginLeft: "30px",
+    width: "630px",
+  },
+};
 
 const mergeStyles = function (posts, config) {
   posts.forEach((post, index) => {
@@ -19,11 +33,18 @@ const mergeStyles = function (posts, config) {
 mergeStyles(trending, trendingConfig);
 mergeStyles(featured, featuredConfig);
 
+const lastFeatured = featured.pop();
+
 const Home = () => {
   return (
     <section className="container home">
       <div className="row">
-        <h2>Trending Post</h2>
+        <h1>Featured Post</h1>
+        <section className="featured-posts-container">
+          <PostMasonry posts={featured} columns={2} tagsOnTop={true} />
+          <MasonryPost post={lastFeatured} tagsOnTop={true} />
+        </section>
+        <h1>Trending Post</h1>
         <PostMasonry posts={trending} columns={3} />
       </div>
     </section>
